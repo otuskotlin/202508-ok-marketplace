8ec69fb121e991f00d31dfbdf075dc93c788beb0
Sergey Proshchaev <95690553+sproshchaev@users.noreply.github.com>
2025-11-12 23:50:57 +0500

    M4l4 monitoring (#20)
    * init
    
    * fix
    
    * workflow файл (CI-build) из m5l5-kafka (202505)

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,29 +19,8 @@ subprojects {
 }
 
 tasks {
-    register("clean") {
-        group = "build"
-        gradle.includedBuilds.forEach {
-            dependsOn(it.task(":clean"))
-        }
-    }
-    val buildInfra: Task by creating {
-        dependsOn(gradle.includedBuild("ok-marketplace-other").task(":buildInfra"))
-    }
-
-//    val buildImages: Task by creating {
-//        dependsOn(gradle.includedBuild("ok-marketplace-be").task(":buildImages"))
-//        mustRunAfter(buildMigrations)
-//    }
-    val e2eTests: Task by creating {
-        dependsOn(gradle.includedBuild("ok-marketplace-tests").task(":e2eTests"))
-//        mustRunAfter(buildImages)
-    }
-
-    register("check") {
+    register("check" ) {
         group = "verification"
         dependsOn(gradle.includedBuild("ok-marketplace-be").task(":check"))
-        dependsOn(e2eTests)
     }
 }
-
